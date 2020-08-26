ed28b75671d3378902ca71e10c9883e1c2c08622
yt8492 <tmymyut2000@gmail.com>
2020-08-26 23:59:28 +0900

    impl post

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,20 @@
 fun main() {
     val server = http.createServer { req, res ->
-        res.write("url: ${req.url}\n")
-        res.write("method: ${req.method}")
-        res.end()
+        if (req.method == "POST") {
+            val bodyBuilder = StringBuilder()
+            req.on("data") { data: Any ->
+                bodyBuilder.append(data.toString())
+            }
+            req.on("end") { ->
+                res.write("url: ${req.url}\n")
+                res.write("body: ${bodyBuilder.toString()}\n")
+                res.end()
+            }
+        } else {
+            res.write("url: ${req.url}\n")
+            res.write("method: ${req.method}")
+            res.end()
+        }
     }
     server.listen(8080)
 }
